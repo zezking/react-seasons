f1ed142ffaa6a2de3f2ef834d63e8ee361bcd261
zezking <zhaoenze001@gmail.com>
2021-05-06 23:56:58 -0700

    update

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 const SeasonDisplay = () => {
   return <div>Season Display</div>;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,38 @@
-//import the react and reactDom libraries
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
 
-//create a react component
+if (module.hot) {
+  module.hot.accept();
+}
 
-//take the react comoponenent and show it on the screen
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // THIS IS THE ONLY TIME we do direct assignment
+    // to this.state
+    this.state = { lat: null, errorMessage: "" };
+  }
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
+    );
+  }
+
+  // React says we have to define render!!
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+
+    return <div>Loading!</div>;
+  }
+}
+
 ReactDOM.render(<App />, document.querySelector("#root"));
