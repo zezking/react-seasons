7047a81d336eb8ef620ac0a47799d618645c6932
zezking <zhaoenze001@gmail.com>
2021-05-09 17:16:07 -0700

    add season display icon

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,5 +1,16 @@
 import React from "react";
+import "./seasonDisplay.css";
 
+const seasonConfig = {
+  summer: {
+    text: "it's summer, hot hot summer",
+    iconName: "sun",
+  },
+  winter: {
+    text: "It's winter so coooold",
+    iconName: "snowflake",
+  },
+};
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
     return lat > 0 ? "summer" : "winter";
@@ -9,8 +20,14 @@ const getSeason = (lat, month) => {
 };
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
-  console.log(season);
-  return <div>Season Display</div>;
+  const { text, iconName } = seasonConfig[season];
+  return (
+    <div className={`season-display ${season}`}>
+      <i className={`massive ${iconName} icon`}></i>
+      <h1>{text}</h1>
+      <i className={`massive ${iconName} icon`}></i>
+    </div>
+  );
 };
 
 export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
-
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -32,7 +31,7 @@ class App extends React.Component {
       return (
         <div>
           Latitude: {this.state.lat}
-          <SeasonDisplay />;
+          <SeasonDisplay />
         </div>
       );
     }
